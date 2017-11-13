7bdc7214a81f45d617d60887777d8c53d870db96
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2017-11-13 14:52:28 +0000

    Add API functionality for orders

--- FirstREST/Controllers/DocVendaController.cs
+++ FirstREST/Controllers/DocVendaController.cs
@@ -19,4 +19,111 @@ namespace FirstREST.Controllers
             return View();
         }
     }
+
+    public class OrdersController : ApiController
+    {
+        public IEnumerable<Lib_Primavera.Model.DocVenda> Get()
+        {
+            return Lib_Primavera.PriIntegration.Encomendas_List();
+        }
+
+        public Lib_Primavera.Model.DocVenda Get(string id)
+        {
+            Lib_Primavera.Model.DocVenda dv = Lib_Primavera.PriIntegration.Encomenda_Get(id);
+            if (dv == null)
+            {
+                throw new HttpResponseException(
+                  Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            else
+            {
+                return dv;
+            }
+        }
+
+
+        public HttpResponseMessage Post(Lib_Primavera.Model.DocVenda dv)
+        {
+            Lib_Primavera.Model.RespostaErro erro = new Lib_Primavera.Model.RespostaErro();
+            erro = Lib_Primavera.PriIntegration.Encomendas_New(dv);
+
+            if (erro.Erro == 0)
+            {
+                var response = Request.CreateResponse(
+                   HttpStatusCode.Created, dv.id);
+                string uri = Url.Link("DefaultApi", new { DocId = dv.id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+        }
+
+
+        public HttpResponseMessage Put(int id, Lib_Primavera.Model.Cliente cliente)
+        {
+
+            Lib_Primavera.Model.RespostaErro erro = new Lib_Primavera.Model.RespostaErro();
+
+            try
+            {
+                erro = Lib_Primavera.PriIntegration.UpdCliente(cliente);
+                if (erro.Erro == 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, erro.Descricao);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, erro.Descricao);
+                }
+            }
+
+            catch (Exception exc)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, erro.Descricao);
+            }
+        }
+
+
+
+        public HttpResponseMessage Delete(string id)
+        {
+
+
+            Lib_Primavera.Model.RespostaErro erro = new Lib_Primavera.Model.RespostaErro();
+
+            try
+            {
+
+                erro = Lib_Primavera.PriIntegration.DelCliente(id);
+
+                if (erro.Erro == 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, erro.Descricao);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, erro.Descricao);
+                }
+
+            }
+
+            catch (Exception exc)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, erro.Descricao);
+
+            }
+
+        }
+
+        [System.Web.Http.HttpGet]
+        public IEnumerable<Lib_Primavera.Model.DocVenda> OrdersCliente(string id)
+        {
+            return Lib_Primavera.PriIntegration.Encomenda_Get_Entidade(id);
+        }
+    }
 }
